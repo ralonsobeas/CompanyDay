968a43cb33d95a26cff489e51d7af03edc752b92
gennakk <44756216+gennakk@users.noreply.github.com>
2022-02-08 13:13:25 +0100

    Base de datos funcional en windows

--- run.py
+++ run.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, url_for, request
+from flaskext.mysql import MySQL
+import urllib
+import urllib.request
+
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
+app.config['MYSQL_DATABASE_DB'] = 'prueba'
+mysql = MySQL()
+mysql.init_app(app)
 
-@app.route("/")
+@app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == "__main__":
-  app.run(debug=True)
+@app.route('/add contact', methods=['POST'])
+def addContact():
+    nombreEmpresa = request.form['nombreEmpresa']
+
+    cur = mysql.get_db().cursor()
+
+    cur.execute("INSERT INTO empresa VALUES ('" + nombreEmpresa + "');")
+    #mysql.connection.commit()
+    mysql.get_db().commit()
+
+    return 'received'
+
+
+app.run(port = 3000, debug=True)
