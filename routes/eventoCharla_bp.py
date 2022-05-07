2225f374ae7f8748932455adffe77e38cf7decf0
oscarbaltasar <oscarbaltasar@hotmail.com>
2022-05-07 14:36:43 +0200

    Añadido registro de charlas y speedmeetings

--- routes/eventoCharla_bp.py
+++ routes/eventoCharla_bp.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from controllers.EventoCharlaController import index, store, show, update, delete,all
-
+from modules.moduleRegistro.moduleRegistro import storeCharla
 """
     Blueprints para EventoCharla
 """
@@ -9,7 +9,7 @@ eventoCharla_bp = Blueprint('eventoCharla_bp', __name__)
 
 #eventoCharla_bp.route('/', methods=['GET'])(index)
 # Guardar EventoCharla
-eventoCharla_bp.route('/crear', methods=['GET','POST'])(store)
+eventoCharla_bp.route('/crear', methods=['GET','POST'])(storeCharla)
 # Mostrar EventoCharla
 #eventoCharla_bp.route('/<int:empresa_id>', methods=['GET','POST'])(show)
 # Actualizar EventoCharla
