0d67d4f02eafc7f7ab10cb9b51b2e5df29b63e22
Hertorias <75383957+Hertorias@users.noreply.github.com>
2022-05-09 08:51:05 +0200

    TEST BBDD 2

--- run.py
+++ run.py
@@ -135,6 +135,9 @@ def login():
     loginForm = LoginForm()
     return dict(loginForm=loginForm)
 
+def testBBDD():
+    empresas = EmpresaController.all_query()
+    return "Primera empresa: " + empresas[0].nombre
 
 # Errores routing
 from errors import *
